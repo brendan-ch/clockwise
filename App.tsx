4d8ac9c8d21057b4e3f4f329c41d5146687e6cfd
Brendan <smallpigeon1@gmail.com>
2021-11-13 21:36:11 -0800

    Install and setup fonts (#7)
    * Add font and app-loading
    
    * Load fonts on startup
    
    * Add globaly styling for font family
    
    * Test global TextStyles

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
+import {
+  useFonts,
+  /* eslint-disable camelcase */
+  AnonymousPro_400Regular,
+  AnonymousPro_400Regular_Italic,
+  AnonymousPro_700Bold,
+  AnonymousPro_700Bold_Italic,
+} from '@expo-google-fonts/anonymous-pro';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TextStyles from './src/styles/Text';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    AnonymousPro_400Regular,
+    AnonymousPro_400Regular_Italic,
+    AnonymousPro_700Bold,
+    AnonymousPro_700Bold_Italic,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={TextStyles.textRegular}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/styles/Text.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const TextStyles = StyleSheet.create({
+  textRegular: {
+    fontFamily: 'AnonymousPro_400Regular',
+  },
+  textItalic: {
+    fontFamily: 'AnonymousPro_400Regular_Italic',
+  },
+  textBold: {
+    fontFamily: 'AnonymousPro_700Bold',
+  },
+  textBoldItalic: {
+    fontFamily: 'AnonymousPro_700Bold_Italic',
+  },
+});
+
+export default TextStyles;
