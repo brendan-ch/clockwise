c359433b8936ef4300daed650bc8cc43f2f84b9c
Brendan <smallpigeon1@gmail.com>
2021-11-13 22:09:35 -0800

    Preview page button bar in app page

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PageButtonBar from './src/components/PageButtonBar';
 import TextStyles from './src/styles/Text';
 
 export default function App() {
@@ -27,6 +28,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={TextStyles.textRegular}>Open up App.tsx to start working on your app!</Text>
+      <PageButtonBar
+        selected="focus"
+        style={styles.pageButtonBar}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -38,5 +43,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  pageButtonBar: {
+    height: 31,
+    width: 268,
   },
 });
