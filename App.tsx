560eaa0a4216a934f95a4fd71a882dcc15ed1043
Brendan <smallpigeon1@gmail.com>
2021-11-14 20:37:21 -0800

    Add text and style to component

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ActionButtonBar from './src/components/ActionButtonBar';
 import PageButtonBar from './src/components/PageButtonBar';
 import TextStyles from './src/styles/Text';
 
@@ -40,6 +41,11 @@ export default function App() {
         // onPressFocus={() => handleStateSwitch('focus')}
         // onPressBreak={() => handleStateSwitch('break')}
       />
+      <ActionButtonBar
+        style={styles.actionButtonBar}
+        text="some text"
+        state="stopped"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -54,6 +60,10 @@ const styles = StyleSheet.create({
   },
   pageButtonBar: {
     height: 31,
+    width: 268,
+  },
+  actionButtonBar: {
+    height: 134,
     width: 268,
   },
 });
