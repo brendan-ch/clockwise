6829fe7326f8fda1a2fbf718a6727291e8ed5039
Brendan <smallpigeon1@gmail.com>
2021-11-16 22:51:02 -0800

    Apply context to app

--- App.tsx
+++ App.tsx
@@ -9,10 +9,22 @@ import {
 import AppLoading from 'expo-app-loading';
 // import AppLoading from 'expo-app-loading';
 // import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Platform } from 'react-native';
+import AppContext from './AppContext';
+import KeyboardShortcutManager from './src/helpers/keyboardShortcutManager';
 import TimerPage from './src/pages/Timer';
 
 export default function App() {
+  const [
+    keyboardShortcutManager, setKeyboardShortcutManager,
+  ] = useState<KeyboardShortcutManager | undefined>(undefined);
+
+  // Memoized context
+  const memoized = useMemo(() => ({
+    keyboardShortcutManager,
+  }), []);
+
   const [fontsLoaded] = useFonts({
     AnonymousPro_400Regular,
     AnonymousPro_400Regular_Italic,
@@ -20,11 +32,21 @@ export default function App() {
     AnonymousPro_700Bold_Italic,
   });
 
+  useEffect(() => {
+    // Initialize keyboard shortcuts on web
+    if (Platform.OS === 'web') {
+      const manager = new KeyboardShortcutManager();
+      setKeyboardShortcutManager(manager);
+    }
+  }, []);
+
   if (!fontsLoaded) {
     return <AppLoading />;
   }
 
   return (
-    <TimerPage />
+    <AppContext.Provider value={memoized}>
+      <TimerPage />
+    </AppContext.Provider>
   );
 }
