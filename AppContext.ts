61f4577dc7526b635e7c3b42f7667e6bc15e3575
Brendan <smallpigeon1@gmail.com>
2021-11-18 17:21:03 -0800

    Update app context

--- AppContext.ts
+++ AppContext.ts
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import KeyboardShortcutManager from './src/helpers/keyboardShortcutManager';
+import { TimerState } from './src/types';
 
 interface DefaultAppState {
   keyboardShortcutManager?: KeyboardShortcutManager,
+  timeRemaining: number,
+  setTimeRemaining: Dispatch<SetStateAction<number>>,
+  timerState: TimerState,
+  setTimerState: Dispatch<SetStateAction<TimerState>>,
+  timeout?: NodeJS.Timeout,
+  setTimeoutState: Dispatch<SetStateAction<NodeJS.Timeout | undefined>>,
 }
 
 const defaultAppState: DefaultAppState = {
   keyboardShortcutManager: undefined,
+  timeRemaining: 0,
+  setTimeRemaining: () => {},
+  timerState: 'stopped',
+  setTimerState: () => {},
+  timeout: undefined,
+  setTimeoutState: () => {},
 };
 
 const AppContext = React.createContext(defaultAppState);
