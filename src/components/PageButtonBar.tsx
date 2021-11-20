dd6a8e413cd4e4a468340d3e4e5b3e8730390dd1
Brendan <smallpigeon1@gmail.com>
2021-11-20 15:45:11 -0800

    Add animated background styling based on selected

--- src/components/PageButtonBar.tsx
+++ src/components/PageButtonBar.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import {
+  Animated,
   StyleProp, StyleSheet, View, ViewStyle,
 } from 'react-native';
+import ColorValues from '../styles/Color';
 import PageButton from './PageButton';
 
 interface Props {
@@ -29,8 +31,42 @@ interface Props {
 function PageButtonBar({
   style, selected, onPressFocus, onPressBreak,
 }: Props) {
+  const widthAnimation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    if (selected === 'focus') {
+      // Run animation timing to left
+
+      Animated.timing(widthAnimation, {
+        toValue: 0,
+        duration: 100,
+        // easing: Easing.back(1),
+        useNativeDriver: true,
+      }).start();
+    } else if (selected === 'break') {
+      // Run animation timing to right
+
+      Animated.timing(widthAnimation, {
+        toValue: 1,
+        duration: 100,
+        // easing: Easing.back(1),
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [selected]);
+
   return (
     <View style={[style, styles.container]}>
+      <View style={styles.backgroundContainer}>
+        <Animated.View style={{
+          // width: `${widthAnimation}%`,
+          // width: widthAnimation,
+          flex: widthAnimation,
+          height: '100%',
+        }}
+        />
+        <View style={styles.backgroundRectangle} />
+      </View>
       <PageButton
         text="focus"
         onPress={onPressFocus}
@@ -58,6 +94,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'stretch',
   },
+  backgroundContainer: {
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    height: '100%',
+    // backgroundColor: '#000000',
+  },
+  backgroundRectangle: {
+    width: '50%',
+    height: '100%',
+    backgroundColor: ColorValues.primary,
+  },
+  // backgroundRectangleSelected: {
+
+  // },
 });
 
 export default PageButtonBar;
