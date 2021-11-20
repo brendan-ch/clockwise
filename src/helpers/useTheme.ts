7dbaa79b5d90d97e3abc6e3312098d7ffae34c61
Brendan <smallpigeon1@gmail.com>
2021-11-19 19:22:32 -0800

    Invert grays on theme change

--- src/helpers/useTheme.ts
+++ src/helpers/useTheme.ts
@@ -3,6 +3,8 @@ import ColorValues from '../styles/Color';
 
 interface Colors {
   primary: string,
+  gray1: string,
+  gray2: string,
   gray3: string,
   gray4: string,
   gray5: string,
@@ -20,6 +22,10 @@ function useTheme(): Colors {
       ...ColorValues,
       primary: '#dbdbdb',
       background: '#000000',
+      gray5: ColorValues.gray1,
+      gray4: ColorValues.gray2,
+      gray2: ColorValues.gray4,
+      gray1: ColorValues.gray5,
     };
   }
 
