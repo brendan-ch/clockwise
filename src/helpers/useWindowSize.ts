6bc26e63065e35a4f31cd467dbf0e2fbaccd0c8d
Brendan <smallpigeon1@gmail.com>
2021-11-21 13:15:24 -0800

    Add a hook for determining window sizes

---
+++ src/helpers/useWindowSize.ts
@@ -0,0 +1,24 @@
+import {
+  useWindowDimensions,
+} from 'react-native';
+
+/**
+ * Hook that returns one of three window sizes.
+ */
+function useWindowSize() {
+  // Get dimensions
+  const { height, width } = useWindowDimensions();
+
+  // Return a size
+  if (height < 400 && width < 700) {
+    return 'small';
+  }
+
+  if (width >= 700) {
+    return 'landscape';
+  }
+
+  return 'portrait';
+}
+
+export default useWindowSize;
