4e0ffcd3132e5747f2fbfb729b141328d907d40f
Brendan <smallpigeon1@gmail.com>
2021-11-21 14:25:07 -0800

    Switch to useLinkProps

--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -1,6 +1,7 @@
 import { Ionicons } from '@expo/vector-icons';
-import { Link } from '@react-navigation/native';
+import { useLinkProps } from '@react-navigation/native';
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 
 interface HeaderButtonProps {
   iconName: string,
@@ -15,21 +16,19 @@ interface HeaderButtonProps {
  * @param props
  */
 function HeaderButton({ iconName, to }: HeaderButtonProps) {
+  const { onPress } = useLinkProps({ to });
+
   return (
-    // <TouchableOpacity
-    //   style={headerButtonStyles.container}
-    //   onPress={onPress}
-    // >
-    <Link
-      to={to}
+    <TouchableOpacity
+      // style={headerButtonStyles.container}
+      onPress={onPress}
     >
       <Ionicons
         // @ts-ignore
         name={iconName}
         size={22}
       />
-    </Link>
-    // </TouchableOpacity>
+    </TouchableOpacity>
   );
 }
 
