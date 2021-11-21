5f3e263fdc8086a392690a0b57318b4878359b38
Brendan <smallpigeon1@gmail.com>
2021-11-21 14:12:36 -0800

    Switch to linking prop

--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -1,40 +1,36 @@
 import { Ionicons } from '@expo/vector-icons';
+import { Link } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
 
 interface HeaderButtonProps {
   iconName: string,
-  onPress?: () => any,
+  to: {
+    screen: string,
+    params: any,
+  },
 }
 
 /**
  * Component representing a left or right header button on mobile.
  * @param props
  */
-function HeaderButton({ iconName, onPress }: HeaderButtonProps) {
+function HeaderButton({ iconName, to }: HeaderButtonProps) {
   return (
-    <TouchableOpacity
-      style={headerButtonStyles.container}
-      onPress={onPress}
+    // <TouchableOpacity
+    //   style={headerButtonStyles.container}
+    //   onPress={onPress}
+    // >
+    <Link
+      to={to}
     >
       <Ionicons
         // @ts-ignore
         name={iconName}
         size={22}
       />
-    </TouchableOpacity>
+    </Link>
+    // </TouchableOpacity>
   );
 }
 
 export default HeaderButton;
-
-HeaderButton.defaultProps = {
-  onPress: () => {},
-};
-
-const headerButtonStyles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
