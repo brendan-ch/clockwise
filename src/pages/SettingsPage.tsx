00e2c9cdaea01218132d601a8a82bc18b93d5eb8
Brendan <smallpigeon1@gmail.com>
2021-11-21 16:39:25 -0800

    Change styling of page

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import useTheme from '../helpers/useTheme';
+import TextStyles from '../styles/Text';
 
 /**
  * Component containing content for the settings page for mobile.
  */
 function SettingsPage() {
+  const colorValues = useTheme();
+
   return (
-    <View>
-      <Text>This is the settings page</Text>
+    <View
+      style={[styles.container, {
+        backgroundColor: colorValues.background,
+      }]}
+    >
+      <Text style={[TextStyles.textRegular, {
+        color: colorValues.primary,
+      }]}
+      >
+        You've found the settings page! Timer customization and account linking are coming soon.
+
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+});
 
 export default SettingsPage;
