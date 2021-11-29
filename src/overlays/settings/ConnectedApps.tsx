31ba3a9715453c9408e1bb5177694eac7dd2de24
Brendan <smallpigeon1@gmail.com>
2021-11-28 16:52:47 -0800

    Add placeholder settings panes

---
+++ src/overlays/settings/ConnectedApps.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ScrollView, Text } from 'react-native';
+
+/**
+ * Connected apps settings in the settings overlay.
+ */
+function ConnectedAppsPane() {
+  return (
+    <ScrollView>
+      <Text>Connected apps settings</Text>
+    </ScrollView>
+  );
+}
+
+export default ConnectedAppsPane;
---
+++ src/overlays/settings/TimerSettings.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ScrollView, Text } from 'react-native';
+
+/**
+ * Timer settings content in the settings overlay.
+ */
+function TimerSettingsPane() {
+  return (
+    <ScrollView>
+      <Text>Timer settings</Text>
+    </ScrollView>
+  );
+}
+
+export default TimerSettingsPane;
