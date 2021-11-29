cc718c7cfc5bee8eafbf569b2209dda76842b6db
Brendan <smallpigeon1@gmail.com>
2021-11-28 17:07:19 -0800

    Update timer settings placeholder

--- src/overlays/settings/TimerSettings.tsx
+++ src/overlays/settings/TimerSettings.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { ScrollView, Text } from 'react-native';
+import useTheme from '../../helpers/useTheme';
+import TextStyles from '../../styles/Text';
 
 /**
  * Timer settings content in the settings overlay.
  */
 function TimerSettingsPane() {
+  const colors = useTheme();
+
   return (
     <ScrollView>
-      <Text>Timer settings</Text>
+      <Text style={[TextStyles.textRegular, {
+        color: colors.primary,
+      }]}
+      >
+        Timer settings (coming soon)
+
+      </Text>
     </ScrollView>
   );
 }
