9141c13403436afaf05bb0fcda7a0c95436ee024
Brendan <smallpigeon1@gmail.com>
2021-11-28 17:05:39 -0800

    Add useTheme hook

--- src/overlays/settings/ConnectedApps.tsx
+++ src/overlays/settings/ConnectedApps.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { ScrollView, Text } from 'react-native';
+// import useTheme from '../../helpers/useTheme';
 
 /**
  * Connected apps settings in the settings overlay.
  */
 function ConnectedAppsPane() {
+  // const colors = useTheme();
+
   return (
     <ScrollView>
       <Text>Connected apps settings</Text>
