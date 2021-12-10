c8aa2887aefa0f2705308dfd9a1ec94af2eab48c
Brendan <smallpigeon1@gmail.com>
2021-12-10 15:25:31 -0800

    Implement landscape footer for future use

--- src/components/LandscapeFooter.tsx
+++ src/components/LandscapeFooter.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, {
+  useContext, useEffect, useRef, useState,
+} from 'react';
+import {
+  StyleSheet, Text, Animated, Platform,
+} from 'react-native';
+import AppContext from '../../AppContext';
+import useTheme from '../helpers/useTheme';
+import TextStyles from '../styles/Text';
 
 function LandscapeFooter() {
+  const sampleText = 'Text';
+  const displayText = false;
+
+  const [hovering, setHovering] = useState(false);
+
+  const opacityAnimation = useRef(new Animated.Value(1)).current;
+  const colorValues = useTheme();
+  const context = useContext(AppContext);
+
+  useEffect(() => {
+    if (context.timerState === 'running' && !hovering) {
+      Animated.timing(opacityAnimation, {
+        toValue: 0,
+        duration: 100,
+        useNativeDriver: false,
+      }).start();
+    } else {
+      Animated.timing(opacityAnimation, {
+        toValue: 1,
+        duration: 100,
+        useNativeDriver: false,
+      }).start();
+    }
+  }, [context.timerState, hovering]);
+
   return (
-    <View style={styles.container} />
+    <Animated.View
+      style={[styles.container, {
+        opacity: opacityAnimation,
+      }]}
+      // @ts-ignore
+      onMouseEnter={Platform.OS === 'web' ? () => setHovering(true) : undefined}
+      onMouseLeave={Platform.OS === 'web' ? () => setHovering(false) : undefined}
+    >
+      <Text style={[TextStyles.textRegular, {
+        color: colorValues.gray4,
+      }]}
+      >
+        {displayText ? sampleText : undefined}
+
+      </Text>
+    </Animated.View>
   );
 }
 
@@ -12,7 +59,8 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 100,
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
