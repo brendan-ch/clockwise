86ef0cedd1b658bd071a2158145a6ad530fa92f7
Brendan <smallpigeon1@gmail.com>
2021-12-11 11:39:58 -0800

    Task selection component (#65)

---
+++ src/components/Selector.tsx
@@ -0,0 +1,119 @@
+import { Ionicons } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import {
+  Platform,
+  ViewStyle,
+  StyleProp, TextStyle,
+  StyleSheet, Pressable, Text, View,
+} from 'react-native';
+import useTheme from '../helpers/useTheme';
+import TextStyles from '../styles/Text';
+
+interface Props {
+  text?: string,
+  subtitle?: string,
+  style?: StyleProp<ViewStyle>,
+  textStyle?: StyleProp<TextStyle>,
+  iconRight?: string,
+  iconLeft?: string,
+  onPressLeft?: () => any,
+  onPressRight?: () => any,
+  onPress?: () => any,
+}
+
+/**
+ * Selector component that is displayed in task list.
+ */
+function Selector({
+  text, iconRight, iconLeft, onPressLeft, onPressRight, onPress, subtitle, textStyle, style,
+}: Props) {
+  const [hovering, setHovering] = useState<'none' | 'text' | 'leftIcon' | 'rightIcon'>('none');
+
+  const colorValues = useTheme();
+
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[styles.container, style]}
+      // @ts-ignore
+      onMouseEnter={Platform.OS === 'web' ? () => setHovering('text') : undefined}
+      onMouseLeave={Platform.OS === 'web' ? () => setHovering('none') : undefined}
+    >
+      {iconLeft ? (
+        <Pressable
+          onPress={onPressLeft}
+          style={[styles.iconContainer, {
+            marginRight: 5,
+          }]}
+          // @ts-ignore
+          onMouseEnter={Platform.OS === 'web' ? () => setHovering('leftIcon') : undefined}
+          onMouseLeave={Platform.OS === 'web' ? () => setHovering('text') : undefined}
+        >
+          {/* @ts-ignore */}
+          <Ionicons name={iconLeft} size={20} color={colorValues.primary} />
+        </Pressable>
+      ) : undefined}
+      <View
+        style={[styles.textContainer]}
+      >
+        <Text style={[TextStyles.textRegular, textStyle, {
+          color: colorValues.primary,
+        }]}
+        >
+          {text}
+
+        </Text>
+        {subtitle ? (
+          <Text>{subtitle}</Text>
+        ) : undefined}
+      </View>
+      {iconRight ? (
+        <Pressable
+          onPress={onPressRight}
+          style={[styles.iconContainer, {
+            marginLeft: 5,
+          }]}
+        >
+          <Ionicons
+            // @ts-ignore
+            name={iconRight}
+            size={20}
+            color={hovering === 'text' ? colorValues.gray3 : colorValues.gray4}
+          />
+        </Pressable>
+      ) : undefined}
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: 50,
+    width: '100%',
+  },
+  textContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  iconContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+Selector.defaultProps = {
+  text: '',
+  subtitle: undefined,
+  iconRight: undefined,
+  iconLeft: undefined,
+  onPressLeft: () => {},
+  onPressRight: () => {},
+  onPress: () => {},
+  textStyle: {},
+  style: {},
+};
+
+export default Selector;
