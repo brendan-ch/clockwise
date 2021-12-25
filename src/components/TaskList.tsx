a3d6ddd965cdb43a1692a24796c229e432204350
Brendan <smallpigeon1@gmail.com>
2021-12-25 14:55:13 -0800

    Task list placeholder (#73)
    * Implement placeholder for empty task list
    
    * Implement placeholder item when timer is running

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -172,6 +172,17 @@ function TaskList() {
         }]}
         />
       ) : undefined}
+      {tasks.length === 0 ? (
+        <Text style={[TextStyles.textRegular, {
+          color: colorValues.gray3,
+          marginTop: 10,
+        }]}
+        >
+          {!['running', 'paused'].includes(context.timerState)
+            ? 'Add some tasks to keep track of them during your session.'
+            : 'No tasks to display.'}
+        </Text>
+      ) : undefined}
       {error ? (
         <Text style={[TextStyles.textRegular, {
           color: colorValues.primary,
