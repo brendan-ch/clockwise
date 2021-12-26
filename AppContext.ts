3a62be9284450e03e011bb7d7eb7edd8e35003f7
Brendan <smallpigeon1@gmail.com>
2021-12-25 22:31:48 -0800

    Remove commented lines from context

--- AppContext.ts
+++ AppContext.ts
@@ -5,11 +5,8 @@ import { KeyboardShortcutGroup, Overlay, TimerState } from './src/types';
 interface DefaultAppState {
   keyboardShortcutManager?: KeyboardShortcutManager,
   timeRemaining: number,
-  // setTimeRemaining: Dispatch<SetStateAction<number>>,
   timerState: TimerState,
-  // setTimerState: Dispatch<SetStateAction<TimerState>>,
   timeout?: number,
-  // setTimeoutState: Dispatch<SetStateAction<any>>,
   mode: 'focus' | 'break',
   /* eslint-disable-next-line */
   handleStateSwitch: (newMode: 'focus' | 'break') => any,
@@ -26,11 +23,8 @@ interface DefaultAppState {
 const defaultAppState: DefaultAppState = {
   keyboardShortcutManager: undefined,
   timeRemaining: 0,
-  // setTimeRemaining: () => {},
   timerState: 'stopped',
-  // setTimerState: () => {},
   timeout: undefined,
-  // setTimeoutState: () => {},
   overlay: 'none',
   setOverlay: () => {},
   keyboardGroup: 'none',
