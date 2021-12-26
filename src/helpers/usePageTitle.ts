33226187b1e41926c891765a980d62ef43ff95f9
Brendan <smallpigeon1@gmail.com>
2021-12-25 22:44:48 -0800

    Update usePageTitle API

--- src/helpers/usePageTitle.ts
+++ src/helpers/usePageTitle.ts
@@ -1,15 +1,16 @@
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Platform } from 'react-native';
-import AppContext from '../../AppContext';
+// import AppContext from '../../AppContext';
+import { TimerState } from '../types';
 import calculateTimerDisplay from './calculateTimer';
 
 /**
  * Hook that provides a method to set the page title. If timer is running,
  * sets the title to the time remaining, concatenated to the page title.
  */
-function usePageTitle(initial: string) {
-  const { timeRemaining, timerState } = useContext(AppContext);
-  const [pageTitle, setPageTitle] = useState(initial || 'Session');
+function usePageTitle(initial: string, timeRemaining: number, timerState: TimerState) {
+  // const { timeRemaining, timerState } = useContext(AppContext);
+  const [pageTitle, setPageTitle] = useState(initial);
 
   useEffect(() => {
     // Run platform check
