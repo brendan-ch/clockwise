3041d630f9c250ad43d5525d565c875fd304eb0f
Brendan <smallpigeon1@gmail.com>
2022-01-28 23:31:21 -0800

    Create clickable text component

---
+++ src/components/ClickableText.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import {
+  Platform, StyleProp, Text, TextStyle,
+} from 'react-native';
+
+interface Props {
+  style: StyleProp<TextStyle>,
+  text: string,
+  onPress?: () => any,
+}
+
+/**
+ * Makes text clickable.
+ * @param props
+ */
+function ClickableText({ style, onPress, text }: Props) {
+  const [hovering, setHovering] = useState(false);
+
+  return (
+    <Text
+      style={[style, {
+        textDecorationLine: hovering ? 'underline' : 'none',
+      }]}
+      onPress={onPress}
+      // @ts-ignore
+      onMouseEnter={Platform.OS === 'web' ? () => setHovering(true) : undefined}
+      onMouseLeave={Platform.OS === 'web' ? () => setHovering(false) : undefined}
+    >
+      {text}
+    </Text>
+  );
+}
+
+ClickableText.defaultProps = {
+  onPress: () => {},
+};
+
+export default ClickableText;
