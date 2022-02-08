c6c469a857826a185d003013a1971eda2564852e
Brendan <smallpigeon1@gmail.com>
2022-02-07 23:03:11 -0800

    Implement getBackground function

---
+++ api/getBackground.ts
@@ -0,0 +1,48 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import mongoose from 'mongoose';
+
+const mongoUri = process.env.MONGO_URI;
+
+// @ts-ignore
+let cached = null;
+interface IBackground {
+  uri: string,
+}
+
+// @ts-ignore
+const Background = mongoose.model<IBackground>('background', { uri: String });
+
+async function connectToDB(uri: string) {
+  // @ts-ignore
+  if (cached !== null) return;
+  cached = await mongoose.connect(uri);
+}
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  if (!mongoUri) {
+    return res.status(500).json({
+      error: 'Unable to connect to database',
+    });
+  }
+
+  await connectToDB(mongoUri);
+
+  const count = await Background.countDocuments();
+  const rand = Math.floor(Math.random() + count - 1);
+
+  const background = await Background.findOne().skip(rand);
+
+  if (!background) {
+    return res.status(404).json({
+      error: 'No background found',
+      rand,
+      count,
+    });
+  }
+
+  return res.status(200).json({
+    uri: background.uri,
+    rand,
+    count,
+  });
+}
