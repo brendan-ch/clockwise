efad7d40f0c5449dfb597399b9ba2cb037dc84e6
Brendan <smallpigeon1@gmail.com>
2022-03-07 17:11:44 -0800

    Support 12-hour time

--- src/helpers/calculateTime.ts
+++ src/helpers/calculateTime.ts
@@ -1,15 +1,27 @@
 /**
  * Given a date object, calculate the time in a nicely-formatted string.
  * @param date
+ * @param type
  */
-function calculateTime(date: Date) {
-  // Use 24-hour time based on setting
-
+function calculateTime(date: Date, type: '12h' | '24h' = '24h') {
   // Calculate the time
-  const hours = date.getHours().toString();
+  let hours = date.getHours();
   const minutes = date.getMinutes();
 
-  return `${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+  let after = '';
+  if (type === '12h' && hours > 11) {
+    after = ' PM';
+  } else if (type === '12h') {
+    after = ' AM';
+  }
+
+  if (type === '12h' && hours > 12) {
+    hours -= 12;
+  } else if (type === '12h' && hours === 0) {
+    hours = 12;
+  }
+
+  return `${hours}:${minutes < 10 ? '0' : ''}${minutes}${after}`;
 }
 
 export default calculateTime;
