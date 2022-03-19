c59c1f768cf64424143e6b0ad752185ecc6174b2
Brendan <smallpigeon1@gmail.com>
2022-03-19 16:28:32 -0700

    Add links and image

---
+++ src/overlays/settings/Apps.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import {
+  Image, StyleSheet, Text, View,
+} from 'react-native';
+import { DARK_MODE_SCREENSHOT, LIGHT_MODE_SCREENSHOT } from '../../Assets';
+import useTheme from '../../helpers/hooks/useTheme';
+import ColorValues from '../../styles/Color';
+import TextStyles from '../../styles/Text';
+
+/**
+ * Settings pane that contains a link to the apps.
+ */
+export default function AppsPane() {
+  const colors = useTheme();
+
+  return (
+    <View style={[styles.container, {
+      backgroundColor: colors.background,
+    }]}
+    >
+      <View style={styles.contentContainer}>
+        <View style={styles.textContainer}>
+          <Text
+            style={[TextStyles.textBold, {
+              color: colors.primary,
+              fontSize: 50,
+            }]}
+          >
+            Download the app
+          </Text>
+        </View>
+        <Image
+          source={
+            colors.background === ColorValues.background
+              ? LIGHT_MODE_SCREENSHOT
+              : DARK_MODE_SCREENSHOT
+          }
+          style={styles.imageContainer}
+        />
+      </View>
+      <View style={[styles.legalContainer]}>
+        <Text style={[TextStyles.textRegular, {
+          color: colors.gray3,
+        }]}
+        >
+          Apple® and the Apple logo® are trademarks of Apple Inc.
+        </Text>
+        <Text
+          style={[TextStyles.textRegular, {
+            color: colors.gray3,
+          }]}
+        >
+          Google Play and the Google Play logo are trademarks of Google LLC.
+
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 31,
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  contentContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textContainer: {
+    width: 300,
+  },
+  imageContainer: {
+    width: 184,
+    height: 377,
+  },
+  legalContainer: {
+
+  },
+});
