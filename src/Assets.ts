e0eeae91831b5fa9be097cdc0c39108fc026fe4b
Brendan <smallpigeon1@gmail.com>
2022-03-19 16:24:27 -0700

    Update asset links

--- src/Assets.ts
+++ src/Assets.ts
@@ -5,3 +5,6 @@ export const SETTINGS_IMAGE = require('../assets/introduction/settings.png');
 export const NO_ADS_IMAGE = require('../assets/introduction/no-ads.png');
 
 export const TIMER_SOUND = require('../assets/timer.mp3');
+
+export const DARK_MODE_SCREENSHOT = require('../assets/app-screenshots/dark-mode-screenshot.png');
+export const LIGHT_MODE_SCREENSHOT = require('../assets/app-screenshots/light-mode-screenshot.png');
