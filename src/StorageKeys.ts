c175283d4237b3daf10164707bbf9c7b78220eb1
Brendan <smallpigeon1@gmail.com>
2022-04-08 09:41:12 -0700

    add SESSION_NUM key

--- src/StorageKeys.ts
+++ src/StorageKeys.ts
@@ -10,9 +10,7 @@ export const LONG_BREAK_ENABLED = '@longBreakEnabled';
  */
 export const AUTO_START_TIMERS = '@autoStartTimers';
 export const TASKS = '@tasks';
-export const START = '@start';
-export const TIMER_LENGTH = '@timerLength';
-export const MODE = '@mode';
+
 export const ENABLE_BACKGROUND_TIMER = '@enableBackgroundTimer';
 export const ENABLE_TIMER_ALERTS = '@enableTimerAlerts';
 export const ENABLE_TIMER_SOUND = '@enableTimerSound';
@@ -20,7 +18,13 @@ export const ENABLE_BACKGROUND = '@enableBackground';
 export const AUTO_APPEARANCE = '@autoAppearance';
 export const DARK_MODE = '@darkMode';
 export const SUPPRESS_INTRODUCTION = '@suppressIntroduction';
-export const SELECTED = '@selectedTasks';
 export const _24_HOUR_TIME = '@24h';
 export const AUTO_START_BREAK = '@autoStartBreak';
 export const AUTO_START_FOCUS = '@autoStartFocus';
+
+// These keys are used for the background timer.
+export const START = '@start';
+export const TIMER_LENGTH = '@timerLength';
+export const MODE = '@mode';
+export const SELECTED = '@selectedTasks';
+export const SESSION_NUM = '@sessionNum';
