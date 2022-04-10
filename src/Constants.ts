cf12d3027567cbc74fddbb8fb92e9b54c8febf5a
Brendan <smallpigeon1@gmail.com>
2022-04-09 20:06:57 -0700

    update release code

--- src/Constants.ts
+++ src/Constants.ts
@@ -25,7 +25,7 @@ export const REGIONS_WITH_12H_TIME = [
 
 // Version and revision numbers
 // Increment revision number on publish to Expo/web
-export const RELEASE_CODE = 'v1.4.0-1';
+export const RELEASE_CODE = '1.4.0-1';
 
 // This is added to export files to indicate compatibility with future versions
 // Export files from previous versions should always be compatible with new ones,
