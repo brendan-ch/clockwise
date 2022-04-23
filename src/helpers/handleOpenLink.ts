5b3393f8855aec306b64fbf08d8aeea062d82821
Brendan <smallpigeon1@gmail.com>
2022-04-22 22:03:38 -0700

    update handleOpenLink function

--- src/helpers/handleOpenLink.ts
+++ src/helpers/handleOpenLink.ts
@@ -1,13 +1,16 @@
-import { Linking, Platform } from 'react-native';
+import { Platform } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 /**
- * Open the link in the in-app web browser, or
- * a new tab on web.
+ * Open the link in the in-app web browser, or redirect
+ * to it on web.
  * @param link
+ * @param newTab
  */
-async function handleOpenLink(link: string) {
-  if (Platform.OS === 'web') {
-    await Linking.openURL(link);
+async function handleOpenLink(link: string, newTab = true) {
+  if (Platform.OS === 'web' && newTab) {
+    window.open(link, '_blank');
+  } else if (Platform.OS === 'web') {
+    window.location.href = link;
   } else {
     await WebBrowser.openBrowserAsync(link, {
       enableBarCollapsing: true,
