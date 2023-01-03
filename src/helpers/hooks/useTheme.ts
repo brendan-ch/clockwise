490cf77ab5a20d4cf0173a6d78164aa7cd81deda
Brendan Chen <smallpigeon1@gmail.com>
2023-01-03 15:20:39 -0800

    listen for app state changes

--- src/helpers/hooks/useTheme.ts
+++ src/helpers/hooks/useTheme.ts
@@ -1,32 +1,39 @@
-import { useContext } from 'react';
-import { useColorScheme } from 'react-native';
+import { useContext, useEffect, useState } from 'react';
+import {
+  Appearance, AppState, AppStateStatus, ColorSchemeName,
+} from 'react-native';
 import SettingsContext from '../../../SettingsContext';
 import { AUTO_APPEARANCE, DARK_MODE } from '../../StorageKeys';
 import ColorValues from '../../styles/Color';
-import { DefaultSettingsState } from '../../types';
-
-interface Colors {
-  primary: string,
-  gray1: string,
-  gray2: string,
-  gray3: string,
-  gray4: string,
-  gray5: string,
-  background: string,
-  backgroundTranslucent: string,
-}
+import { Colors, DefaultSettingsState } from '../../types';
 
 /**
  * Hook that automatically returns a color scheme based on device theme settings.
  */
 function useTheme(providedContext?: DefaultSettingsState): Colors {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] = useState<ColorSchemeName>('light');
+
+  useEffect(() => {
+    function changeColorScheme(state: AppStateStatus) {
+      if (state === 'active') {
+        // Update the color scheme
+        const newColorScheme = Appearance.getColorScheme();
+        setColorScheme(newColorScheme);
+      }
+    }
+
+    AppState.addEventListener('change', changeColorScheme);
+
+    return () => AppState.removeEventListener('change', changeColorScheme);
+  }, []);
 
   let context = useContext(SettingsContext);
   if (providedContext) {
     context = providedContext;
   }
 
+  // User's setting overrides system appearance
   if (
     (colorScheme === 'dark' && context[AUTO_APPEARANCE])
     || (context[DARK_MODE] && !context[AUTO_APPEARANCE])
